343808c34dbc59a51c2b3d128ba70f60ded4cc8a
xhan-Chen <eeyxc7@nottingham.ac.uk>
2026-02-09 14:52:52 +0000

    Clear files from master

--- Worksheet3/hello.cpp
+++
@@ -1,7 +0,0 @@
-// begin = hello.cpp========== =
-#include <iostream>
- int main(int argc, char *argv[]) {
-	 std::cout << "Hello Planet Earth!" << std::endl;
-	 return 0;
-}
- // end= hello.cpp============
