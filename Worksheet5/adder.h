c81d934e05f0f2517351188952d811b62af88e16
xhan-Chen <eeyxc7@nottingham.ac.uk>
2026-02-10 14:07:18 +0000

    Clean gh-pages branch

---
+++ Worksheet5/adder.h
@@ -0,0 +1,17 @@
+// adder.h
+ #ifndef MATHSLIBADDERH
+#define MATHSLIBADDERH
+
+ /** @file
+ * This file contains the declarations of all exported functions in the maths library.
+*/
+
+ /** Adding function [Brief description]
+ * This function adds two numbers [More detail ]
+ * @parama is the first number [Parameter definition]
+ * @paramb is the second number [Parameter definition]
+* @return sum of a and b [Return value description]
+ */
+ int add(int a, int b);
+
+#endif
