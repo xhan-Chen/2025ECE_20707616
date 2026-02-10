cb853b69bdff36798daf3abbbb86dd327065523f
xhan-Chen <eeyxc7@nottingham.ac.uk>
2026-02-10 11:03:43 +0000

    Complete Exercise 1: Basic CMake setup

---
+++ Worksheet4/hello_cmake/hello.cpp
@@ -0,0 +1,8 @@
+// begin =hello.cpp========== =
+#include <iostream>
+ int main(int argc, char *argv[]) {
+	std::cout << "Hello Planet Earth!" << std::endl;
+	return 0;
+	
+}
+ // end=hello.cpp============ =
